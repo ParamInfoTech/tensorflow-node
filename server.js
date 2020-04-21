e7375a21d8d77ed0866ac14322c33167b85ee080
ParamInfoTech <38746179+ParamInfoTech@users.noreply.github.com>
2020-04-21 20:47:53 +0530

    5

---
+++ server.js
@@ -0,0 +1,40 @@
+let express = require("express");
+let os = require("os");
+let path = require("path");
+
+let app = express();
+let port = process.env.PORT || 3000;
+
+app.use("/html", express.static("./public"));
+app.use("/node", express.static("./node_modules"));
+
+app.get("/", (req, res) => {
+    let filePath = path.join(__dirname, "public/MemoryManagement_2.html");
+    //let filePath = path.join("/html/MemoryManagement_2.html");
+    res.sendFile(filePath, (err) =>{
+        if(err){
+            console.log(err);
+        }else{
+            console.log("File : " + filePath + " Sent...");
+        }
+    });
+});
+
+app.get("/Test2", (req, res) =>{
+    let filePath = path.join(__dirname, "public/MemoryManagement.html");
+    //let filePath = path.join("/html/MemoryManagement_2.html");
+    res.sendFile(filePath, (err) =>{
+        if(err){
+            console.log(err);
+        }else{
+            console.log("File : " + filePath + " Sent...");
+        }
+    });
+
+    //filePath = path.join(__dirname, "public/kc_house_data.csv");
+    //res.download(filePath, "test.csv");
+});
+
+app.listen(port, () => {
+    console.log("Server listening on PORT number : " + port);
+});
